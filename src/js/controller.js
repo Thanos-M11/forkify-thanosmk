799e5138231ee6703b16d6af43f648b19f4eb805
Thanos-M11 <kalaitzisa@gmail.com>
2024-02-05 20:42:42 +0200

    Addeed new welcome feture

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipy) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the Applicatoin!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,7 +136,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 
 init();
